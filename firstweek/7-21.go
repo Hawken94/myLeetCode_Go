5097cf24e210ab3865be7fe38d77629a8b6eb4c6
hawken94 <105847116@qq.com>
2017-07-21 20:31:22 +0800

    update

---
+++ firstweek/7-21.go
@@ -0,0 +1,17 @@
+// Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero
+
+// 给定n个整数的数组S，S中有元素a，b，c，使得a+b+c = 0？查找数组中所有独特的三元组，它们的总和为零  (三元组不能重复)
+
+// For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+// A solution set is:
+// [
+//   [-1, 0, 1],
+//   [-1, -1, 2]
+// ]
+
+package firstweek
+
+func threeSum(nums []int) [][]int {
+
+}
