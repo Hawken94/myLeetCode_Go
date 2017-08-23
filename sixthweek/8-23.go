f2cf8072874cb1812a406526bcb1bb0a9ddb71a6
Hawken94 <1058471169@qq.com>
2017-08-24 00:00:47 +0800

    添加描述

--- sixthweek/8-23.go
+++ sixthweek/8-23.go
@@ -1,5 +1,10 @@
 package sixthweek
 
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+//字符串"PAYPALISHIRING"被以给定行的曲折形式写成了下面的样子：(有时候为了方便阅读你可能需要以固定的字体写成这种样子) 注：从上往下再从下往上读再
+
+// convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
 // Convert ZigZag Conversion
 func Convert(s string, numRows int) string {
 	if len(s) < 0 || numRows <= 1 {
