0763fcd49352f68a653fa2acf1feadc9d5ab6afb
Hawken94 <1058471169@qq.com>
2018-01-23 23:20:33 +0800

    format file

--- december/decFifthweek/Merge_Sorted_Array.go
+++ december/decFifthweek/Merge_Sorted_Array.go
@@ -2,8 +2,15 @@ package decFifthweek
 
 // date:2017-12-28
 
-// 题目:Merge Sorted Array
+// 题目: Merge Sorted Array
+// 合并已排序数组
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+*/
 
+// 如果不返回一个引用就可以使用一个临时变量了
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	i, j, k := m-1, n-1, m+n-1
 	for i >= 0 && j >= 0 {
